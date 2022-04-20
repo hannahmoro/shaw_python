a35ffd7d28a8dd41788f64d8fdcc1381598cce2f
hannahmo-hc <hannah.moshontz@huntclub.com>
2022-04-20 14:30:42 -0500

    fix typo in ex2.py

--- ex2.py
+++ ex2.py
@@ -3,7 +3,7 @@
 
 print("I could have code like this.") # and the comment after is ignored
 
-# You can also use a comment to "disable" of comment out code:
+# You can also use a comment to "disable" or comment out code:
 # print ("This won't run.")
 
 print("This will run.")
