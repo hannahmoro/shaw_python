9051b489af4921179d4002e84d5ab77c7509a94c
hannahmo-hc <hannah.moshontz@huntclub.com>
2022-04-25 13:11:22 -0500

    add exercise 3

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:") #prints message
+
+print("Hens", 25 + 30 / 6) #divides 30 by 6 then adds 25
+print("Roosters", 100 - 25 * 3 % 4) # multiplies 25 by 3, takes mod 4, subtracts from 100
+
+print("Now I will count the eggs:") #prints message
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# divides 1/4, and takes 4 mod 2, then adds everything
+
+print("Is it true that 3 + 2 < 5 - 7?") #print message
+
+print(3 + 2 < 5 - 7) #evaluate each side then compare
+
+print("What is 3 + 2?", 3 + 2) # maths, then prints
+print("What is 5 - 7?", 5 - 7) # ditto
+
+print("Oh, what's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2) #compares according to operation
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
