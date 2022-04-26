a59f9eea78f7eb4e494300db7d4aa5aa84e4d8fe
hannahmo-hc <hannah.moshontz@huntclub.com>
2022-04-26 11:49:38 -0500

    added annotations for exercise 19

--- ex19.py
+++ ex19.py
@@ -5,17 +5,17 @@ def cheese_and_crackers(cheese_count, boxes_of_crakers):
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
-print("we can just give the function numbers directly:")
-cheese_and_crackers(20, 30)
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30) #uses the function with 20 cheeses and 30 boxes of crackers
 
-print("OR, we can use variables from our script:")
+print("OR, we can use variables from our script:") #message, then sets variables
 amount_of_cheese = 10
 amount_of_crackers = 50
 
-cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+cheese_and_crackers(amount_of_cheese, amount_of_crackers) #uses function with set variables
 
 print("We can even do math inside, too:")
-cheese_and_crackers(10 + 20, 5 + 6)
+cheese_and_crackers(10 + 20, 5 + 6) #uses function with values added together
 
-print("And we can combine the two, variables and math:")
+print("And we can combine the two, variables and math:") #message then uses function with vars plus values
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
