ccf7d68b55afc623a4aba975703092bde91e6d6d
hannahmo-hc <hannah.moshontz@huntclub.com>
2022-05-17 19:38:51 -0500

    ex23.py

--- ex23.py
+++ ex23.py
@@ -1,19 +1,20 @@
-import sys
-script, input_encoding, error = sys.argv
+import sys #importing sys modules
+script, input_encoding, error = sys.argv #taking in input_encoding and error from command args
 
-def main(language_file, encoding, errors):
-    line = language_file.readline()
-    if line:
-        print_line(line, encoding, errors)
-        return main(language_file, encoding, errors)
+def main(language_file, encoding, errors): #defining main function which takes these args
+    line = language_file.readline()  #reads one line
+    if line: #unless line doesn't exist (we are at the end of the file)
+        print_line(line, encoding, errors) #use the print line function 
+        return main(language_file, encoding, errors) #
     
-def print_line(line, encoding, errors):
-    next_lang = line.strip()
+def print_line(line, encoding, errors): #defines print line function
+    next_lang = line.strip() #
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
 
 languages = open("languages.txt", encoding= "utf-8")
+
 
 main(languages, input_encoding, error)
